3bf71be68d897e32920ef7238e57038b9c1b1a6c
hollali <dheztinykartel@gmail.com>
2023-12-28 14:54:47 +0000

    Add react-icons, update package.json, add icons to Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,29 @@
+import Link from 'next/link';
+import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
 const Header = () => {
     return  (
     <header className="py-10 px-4 container mx-auto text xl flex flex-nowrap items-center justify-between">
-        <div></div>
-        <ul></ul>
+        <div className="flex items-center w-full md:2/3">
+            <link href="/"className="font-black text-[#F27405">
+                SKY
+            </link>
+            <ul className="flex items-center ml-5">
+                <li className='flex items-center'>
+                <link href="/auth">
+                <FaUserCircle  className='cursor-pointer'/>
+                </link>
+                </li>  
+                <li className='ml-2'>
+                    <MdDarkMode className='cursor-pointer' />
+                </li>
+            </ul>
+        </div>
+        <ul className='flex-item-center justify-between w-full md:1/3 mt-4'>
+            <li className='hover:translate-y-2 duration-500 transition-all'>
+                <link href='/'>Home</link>
+            </li>
+        </ul>
     </header>);
 };
 
