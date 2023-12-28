9a50ddbcc3ffebaeb0204c77885390c24adde4b3
hollali <dheztinykartel@gmail.com>
2023-12-28 15:02:01 +0000

    Revert "Remove Header component"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
-import Header from '@/app/components/Header';
+import Header from '@/app/components/Header/Header';
 import './globals.css'
 
 const poppins = Poppins({ 
