95e1fac1f0e2b639b57f250b37c845bc254a80ed
hollali <dheztinykartel@gmail.com>
2023-12-29 08:55:05 +0000

    Update header links and icons

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,30 +1,37 @@
 import Link from 'next/link';
-import { FaUserCircle } from "react-icons/fa";
-import { MdDarkMode } from "react-icons/md";
+import { FaUserCircle } from 'react-icons/fa';
+import { MdDarkMode } from 'react-icons/md';
 const Header = () => {
     return  (
     <header className="py-10 px-4 container mx-auto text xl flex flex-nowrap items-center justify-between">
         <div className="flex items-center w-full md:2/3">
-            <link href="/"className="font-black text-[#F27405">
+            <Link href="/"className='font-black text-[#F27405'>
                 SKY
-            </link>
+            </Link>
             <ul className="flex items-center ml-5">
                 <li className='flex items-center'>
-                <link href="/auth">
+                <Link href="/auth">
                 <FaUserCircle  className='cursor-pointer'/>
-                </link>
+                </Link>
                 </li>  
                 <li className='ml-2'>
                     <MdDarkMode className='cursor-pointer' />
                 </li>
             </ul>
         </div>
-        <ul className='flex-item-center justify-between w-full md:1/3 mt-4'>
+        <ul className='flex-item-center justify-between w-full md:w-1/3 mt-4'>
             <li className='hover:translate-y-2 duration-500 transition-all'>
-                <link href='/'>Home</link>
+                <Link href='/'>Home</Link>
+            </li>
+            <li className='hover:translate-y-2 duration-500 transition-all'>
+                <Link href='/rooms'>Rooms</Link>
+            </li>
+            <li className='hover:translate-y-2 duration-500 transition-all'>
+                <Link href='/'>Home</Link>
             </li>
         </ul>
-    </header>);
+    </header>
+    );
 };
 
 export default Header;
