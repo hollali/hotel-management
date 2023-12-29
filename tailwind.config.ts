f22cdb27764c74e220e1f93c585573a2b18319e4
hollali <dheztinykartel@gmail.com>
2023-12-29 12:39:47 +0000

    Updated header component and added gradient color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          '--tw-gradient-stops': 'rgba(255, 0, 0, 1), rgba(0, 0, 255, 1)',
       },
     },
   },
