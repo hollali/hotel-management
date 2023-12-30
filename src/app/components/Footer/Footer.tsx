92e825f71dbca4fe778ea02c0d470e5d106fa282
hollali <dheztinykartel@gmail.com>
2023-12-30 03:40:50 +0000

    Add Footer

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <footer className="mt-16">
+            <div className="container mx-auto px-4">
+                <Link href="/"className="font-black text-tertiary-dark">
+                    SKY
+                </Link>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
