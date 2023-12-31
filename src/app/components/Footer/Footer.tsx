7d16cf52d3ab1281421b5498c49831f5a503876a
hollali <dheztinykartel@gmail.com>
2023-12-31 10:15:14 +0000

    Change the link color in the footer from `text-tertiary-dark` to `text-tertiary-light`.

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="mt-16">
             <div className="container mx-auto px-4">
-                <Link href="/"className="font-black text-tertiary-light">
+                <Link href="/" className="font-black text-tertiary-light">
                     SKY
                 </Link>
             </div>
