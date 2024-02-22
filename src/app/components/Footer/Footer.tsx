24fb0db1c621a28ec4697cb9360e7da680c65033
hollali <dheztinykartel@gmail.com>
2024-02-22 20:09:40 +0000

    Add contact information to the footer.

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import {BsFillSendFill} from "react-icons/bs"
 const Footer = () => {
     return (
         <footer className="mt-16">
@@ -7,6 +7,17 @@ const Footer = () => {
                 <Link href="/" className="font-black text-tertiary-light">
                     SKY
                 </Link>
+                <h4 className="font-semibold text-[40px] py-6">Contact</h4>
+
+                <div className="flex flex-wrap gap-16 items-center justify-between">
+                    <div className="flex-1">
+                        <p>Accra Ghana</p>
+                        <div className="flex items-center py-4">
+                            <BsFillSendFill />
+                            <p className="ml-2">Hollali</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     );
