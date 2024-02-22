82c4ba423a02f4b738ffd8364fdf1de97780afd0
hollali <dheztinykartel@gmail.com>
2024-02-22 21:53:01 +0000

    Add theme context.

---
+++ src/app/context/themeContext.tsx
@@ -0,0 +1,10 @@
+import { Dispatch, SetStateAction, createContext} from "react";
+
+type ThemeContextType = {
+    darkTheme: boolean;
+    setDarkTheme: Dispatch<SetStateAction<boolean>>;
+};
+const themeContext = createContext<ThemeContextType>({
+    darkTheme: false,
+    setDarkTheme: () => null,
+});
