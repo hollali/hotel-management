86954b9c975107c1c3573a056d03435cfd72edee
hollali <dheztinykartel@gmail.com>
2024-02-23 14:24:09 +0000

    Implement dark mode toggle feature.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,8 +1,14 @@
-import Link from 'next/link';
-import { FaUserCircle } from 'react-icons/fa';
-import { MdDarkMode } from 'react-icons/md';
+'use client';
 
+import Link from 'next/link';
+import { useContext } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { MdDarkMode ,MdOutlineLightMode} from 'react-icons/md';
+
+import ThemeContext from '@/app/context/themeContext';
 const Header = () => {
+    const { darkTheme, setDarkTheme} = useContext(ThemeContext)
+
     return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-nowrap items-center justify-between">
         <div className="flex items-center w-full md:w-2/3">
@@ -12,11 +18,26 @@ const Header = () => {
         <ul className="flex items-center ml-5">
         <li className="flex items-center">
             <Link href="/auth">
-            <FaUserCircle className="cursor-pointer" />
+            <FaUserCircle className="cursor-pointer" 
+            />
             </Link>
         </li>
         <li className="ml-2">
-            <MdDarkMode className="cursor-pointer" />
+            {darkTheme ? (
+                <MdOutlineLightMode className="cursor-pointer"
+                onClick={() =>{
+                    setDarkTheme(false);
+                    localStorage.removeItem("hotel-theme")
+                }}
+                />
+                ): (
+                <MdDarkMode className="cursor-pointer" 
+            onClick={() =>{
+                setDarkTheme(true);
+                localStorage.setItem("hotel-theme","true")
+            }}
+            />
+            )}
         </li>
         </ul>
     </div>
