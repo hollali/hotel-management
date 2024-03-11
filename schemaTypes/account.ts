0b7078ca636e0f384d0f60104ae8d63a7171c4f6
hollali <dheztinykartel@gmail.com>
2024-03-11 10:18:53 +0000

    created account.ts file

---
+++ schemaTypes/account.ts
@@ -0,0 +1,35 @@
+import { defineField } from "sanity";
+
+const account = {
+    name: 'account',
+    title: 'Account',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'providerType',
+            type: 'string',
+        }),
+        defineField({
+            name: 'providerId',
+            type: 'string',
+        }),
+        defineField({
+            name: 'providerAccountId',
+            type: 'string',
+        }),
+        defineField({
+            name: 'refreshToken',
+            type: 'string',
+        }),
+        defineField({
+            name: 'accessToken',
+            type: 'string',
+        }),
+        defineField({
+            name: 'accessTokenExpires',
+            type: 'number',
+        }),
+    ],
+};
+
+export default account;
