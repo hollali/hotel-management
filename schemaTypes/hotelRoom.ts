3f50456d2ab7d86bec92095bdc89b046530baf07
hollali <dheztinykartel@gmail.com>
2024-03-12 21:27:45 +0000

    Added hotelRoom.ts file into the schemaTypes directory

---
+++ schemaTypes/hotelRoom.ts
@@ -0,0 +1,23 @@
+import { defineField } from "sanity";
+
+const hotelRoom ={
+    name: 'hotelRoom',
+    title: 'Hotel Room',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+            validation: Rule => Rule.required().max(50).error('Maximum 50 characters'),
+        }),
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source:'name',
+            },
+            validation: Rule => Rule.required(),
+        }),
+    ],
+};
