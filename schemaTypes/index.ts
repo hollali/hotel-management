878473bd0a4b6dda44354bd98ebad13e9e5e7da9
hollali <dheztinykartel@gmail.com>
2024-03-13 20:12:42 +0000

    Add `booking` and `hotelRoom` to `schemaTypes`

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -5,4 +5,11 @@ import review from './review';
 import user from './user';
 import verificationToken from './verificationToken';
 
-export const schemaTypes = [user, account,booking,hotelRoom,review,verificationToken];
+export const schemaTypes = [
+    user, 
+    account,
+    booking,
+    hotelRoom,
+    review,
+    verificationToken,
+];
