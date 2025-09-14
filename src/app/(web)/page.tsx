bf27c0a107b6c749cc6bad8173fa8c9560b6842f
hollali <dheztinykartel@gmail.com>
2025-09-14 10:44:45 +0000

    `Update Home component with new layout and content`

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <>Home</>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">
+        Welcome to the Sky Inn we are glad to have you here!
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
