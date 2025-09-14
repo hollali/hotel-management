2e6146b57a73c7c9a18581671d83575c7f7974f7
hollali <dheztinykartel@gmail.com>
2025-09-14 07:13:45 +0000

    `feat: added react-hot-toast and updated package-lock.json and package.json`

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -4,35 +4,38 @@ import Header from "@/app/components/Header/Header";
 import "./globals.css";
 import Footer from "../components/Footer/Footer";
 import ThemeProvider from "../components/ThemeProvider/ThemeProvider";
+import { NextAuthProvider } from "../components/AuthProvider/AuthProvider";
 
 const poppins = Poppins({
-	subsets: ["latin"],
-	weight: ["400", "500", "700", "900"],
-	style: ["italic", "normal"],
-	variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
-	title: "Sky Hotel",
-	description: "The Premier Hotel In Ghana",
+  title: "Sky Inn Hotel",
+  description: "The Premier Hotel In Ghana",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={poppins.className}>
-				<ThemeProvider>
-					<main className="font-normal">
-						<Header />
-						{children}
-						<Footer />
-					</main>
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <NextAuthProvider>
+          <ThemeProvider>
+            <main className="font-normal">
+              <Header />
+              {children}
+              <Footer />
+            </main>
+          </ThemeProvider>
+        </NextAuthProvider>
+      </body>
+    </html>
+  );
 }
